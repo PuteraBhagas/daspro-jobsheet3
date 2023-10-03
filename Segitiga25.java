2d637ebc7c837b2f8f85898235ddc3a63612e57c
PuteraBhagas <p.bhagaswara@gmail.com>
2023-10-03 16:28:53 +0700

    ini git hub ke sekian saya

---
+++ Segitiga25.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Segitiga25
+ */
+public class Segitiga25 {
+
+    public static void main(String[] args) {
+        // Deklarasi Scanner
+        Scanner sc = new Scanner (System.in);
+        
+        // Deklarasi Variabel
+        int alas, tinggi;
+        float luas;
+
+        // Syntax input
+        System.out.println("Masukkan alas: ");
+        alas = sc.nextInt();
+        System.out.println("Masukkan tinggi: ");
+        tinggi = sc.nextInt();
+
+        // Syntax untuk menghitung luas segitiga
+        luas = alas* tinggi / 2;
+        System.out.println("Luas Segitiga:"+luas);
+    }
+}
